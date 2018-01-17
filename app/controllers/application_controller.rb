2ea9ca7f4eb51d58133f260d53e390cb0934de4e
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-17 02:04:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
   get '/' do
       erb :index
     end
-  
+
     post '/login' do
       @user = User.find_by(:username => params[:username])
       if @user != nil && @user.password == params[:password]
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
       end
       erb :error
     end
-  
+
     get '/account' do
       @current_user = User.find_by_id(session[:user_id])
       if @current_user
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
         erb :error
       end
     end
-  
+
     get '/logout' do
       session.clear
       redirect to '/'
